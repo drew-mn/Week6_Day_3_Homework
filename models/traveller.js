dd0a75aa6d7b9098db87ab27050eaee5a37befd9
Drew Neillie <drew.neillie@live.co.uk>
2018-10-10 21:41:49 +0100

    MVP and one extension completed. 11 passing tests

--- models/traveller.js
+++ models/traveller.js
@@ -3,31 +3,37 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+return this.journeys.map(journey => journey.startLocation);
 };
 
-Traveller.prototype.getJourneyEndLocations = function () {
 
+Traveller.prototype.getJourneyEndLocations = function () {
+return this.journeys.map(journey => journey.endLocation);
 };
 
 Traveller.prototype.getModesOfTransport = function () {
-
+return this.journeys.map(modes => modes.transport);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+return this.journeys.filter(journey => journey.distance > minDistance)
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+// assign two parameters (accumulator, currentValue)
+// accumulator =  total | currentValue = journey
+  return this.journeys.reduce((totalDistance, journey) => {
+    // return accumulator(running total) += distance traveled
+    return totalDistance += journey.distance;
+    // add a second argument of 0 after callback to ensure distances are calculated from starting point of 0.
+  },0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  
 };
 
 
